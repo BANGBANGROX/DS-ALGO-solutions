4814a4dcbad4b8179d8255c8e86157437d1c8736
BANGBANGROX <lakshya0809bang@gmail.com>
2022-08-10 00:35:00 +0530

    Minor change

--- Number_of_paths_in_a_matrix_with_k_coins.cpp
+++ Number_of_paths_in_a_matrix_with_k_coins.cpp
@@ -35,18 +35,16 @@ public:
 
 //{ Driver Code Starts.
 
-int main()
-{
+int main() {
     Solution obj;
-    int i,j,k,l,m,n,t;
-    cin>>t;
-    while(t--)
-    {
-        cin>>k>>n;
-        vector<vector<int>> v(n+1, vector<int>(n+1, 0));
-        for(i=0;i<n;i++)
-            for(j=0;j<n;j++)
-                cin>>v[i][j];
+    int i, j, k, n, t;
+    cin >> t;
+    while (t--) {
+        cin >> k >> n;
+        vector<vector<int>> v(n + 1, vector<int>(n + 1, 0));
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                cin >> v[i][j];
         cout << obj.numberOfPath(n, k, v) << endl;
     }
 }
